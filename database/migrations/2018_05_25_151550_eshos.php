c088bf35f8763f3a386b0e3f1d794cff30d8ca5a
Nuur Haider <haidernuur@gmail.com>
2018-05-26 03:41:42 +0600

    Tinker ota hiklam

--- database/migrations/2018_05_25_151550_eshos.php
+++ database/migrations/2018_05_25_151550_eshos.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEshoTable extends Migration
+class Eshos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateEshoTable extends Migration
      */
     public function up()
     {
-        Schema::create('esho', function (Blueprint $table) {
+        Schema::create('eshos', function(Blueprint $table){
             $table->increments('id');
+            $table->string('kobe');
+            $table->string('kuthay');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateEshoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('esho');
+         Schema::dropIfExists('eshos');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\esho;
 Route::get('/', function () {
     return view('welcome'
     );
 });
 
 Route::get('about', function(){
-	$name = 'deepika padukune';
-	$tor_khams = [
-		'chole ashba',
-		'amake khushi korba',
-		'khusi hole $ niye chole jaba'
-	];
-	return view('about', compact(
-	'name','tor_khams'));
+	$esho = 
+	$tumra_jara_chole_ashar_kotha = DB::table('users')->get();
+	return $tumra_jara_chole_ashar_kotha;
+	// return view('about', compact(
+	// 'name','tor_khams'));
 });
+
+// Route::get('eshos/{esho}', function($id){
+// 	$esho = App\esho::find($id);
+	
+// 	return view('eshos.show', compact('esho'));
+
+// });
